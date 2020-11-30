a819bb93c755603fd42bb1ed3d997314b74f4ef3
seongsangCHO <secho@student.42seoul.kr>
2020-11-30 22:12:34 +0900

    current os theme apply

--- src/main.js
+++ src/main.js
@@ -8,9 +8,19 @@ DOM.toggleButton.innerHTML = "Hello";
 
 /* Status */
 
-const STATUS = {
+const status = {
   isToggled: true,
 };
+
+status.currentTheme = localStorage.getItem("theme");
+status.preferDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentThemeApply = () => {
+  if (status.currentTheme == "dark") document.body.classList.toggle("dark");
+  else if (status.currentTheme == "light")
+    document.body.classList.toggle("light");
+};
+currentThemeApply();
 
 DOM.toggleButton.addEventListener("click", () => {
   document.body.classList.toggle("dark");
