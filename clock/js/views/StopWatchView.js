79ff45fae42217b2f54e1b4360f792d2b7c8cdf7
seongsangCHO <secho@student.42seoul.kr>
2020-12-23 16:26:07 +0900

    ms time

--- clock/js/views/StopWatchView.js
+++ clock/js/views/StopWatchView.js
@@ -9,15 +9,22 @@ StopWatchView.setup = function (el) {
   this.init(el);
   return this;
 };
+/*
+모델에서 localStorage 데이터 가져오기
+시간관련 로직은 여기서 짜기
+setup시킬때 getLocalStorage
+*/
 StopWatchView.stopWatchWorker = function () {
   console.log(tag, "stopWatchWorker");
   let content = this.getLocalStorage();
-  this.clockEl.innerText = "hh";
+  this.clockEl.innerHTML = `00:00:00<span class="clock-ms">.00</span>`;
 };
 
 StopWatchView.getLocalStorage = function () {
-  if (!localStorage.time) {
+  if (!localStorage.getItem("prevStopWatchTime")) {
+    localStorage.setItem("prevStopWatchTime",0);
   }
+  return localStorage.getItem("prevStopWatchTime")
 };
 
 export default StopWatchView;
