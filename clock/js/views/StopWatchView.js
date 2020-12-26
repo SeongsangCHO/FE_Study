8a016093e3630f7f3d1534278f3279b3b9ee4b30
seongsangcho <liasqui31@google.com>
2020-12-26 16:55:09 +0900

    Stopwatch setInterval

--- clock/js/views/StopWatchView.js
+++ clock/js/views/StopWatchView.js
@@ -1,14 +1,24 @@
-import ResultView from "./ClockView.js";
+import ClockView from "./ClockView.js";
 
 const tag = "[StopWatchView.js]";
 
-const StopWatchView = Object.create(ResultView);
+const StopWatchView = Object.create(ClockView);
 /* 새로고침해도 계속 수행해야함. */
 
 StopWatchView.setup = function (el) {
   this.init(el);
+  this.getPrevTime();
   return this;
 };
+
+StopWatchView.getPrevTime = function(){
+  this.ms = localStorage.getItem("ms") || 0;
+  this.ss = localStorage.getItem("ss") || 0;
+  this.mm = localStorage.getItem("mm") || 0;
+  this.hh = localStorage.getItem("hh") || 0;
+  this.dd = localStorage.getItem("dd") || 0;
+}
+
 /*
 모델에서 localStorage 데이터 가져오기
 시간관련 로직은 여기서 짜기
@@ -18,13 +28,17 @@ StopWatchView.stopWatchWorker = function () {
   console.log(tag, "stopWatchWorker");
   let content = this.getLocalStorage();
   this.clockEl.innerHTML = `00:00:00<span class="clock-ms">.00</span>`;
+  const updateTime = () =>{
+    console.log('ㅗㅑ');
+  }
+  this.isInterval = setInterval(updateTime, 1);
 };
 
 StopWatchView.getLocalStorage = function () {
   if (!localStorage.getItem("prevStopWatchTime")) {
-    localStorage.setItem("prevStopWatchTime",0);
+    localStorage.setItem("prevStopWatchTime", 0);
   }
-  return localStorage.getItem("prevStopWatchTime")
+  return localStorage.getItem("prevStopWatchTime");
 };
 
 export default StopWatchView;
