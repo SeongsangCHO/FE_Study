1209bcf3e6d6e2fd80d4ec5af4c29da9745ae1fd
seongsangcho <liasqui31@google.com>
2021-01-10 23:04:36 +0900

    feat페이지 진입 시 포커스가 input 에 가도록 처리하고, 키워드를 입력한 상태에서 input 을 클릭할 시에는 기존에 입력되어 있던 키워드가 삭제되도록 만들어야 합니다.

--- programmers_FE_SUBJECT/src/SearchInput.js
+++ programmers_FE_SUBJECT/src/SearchInput.js
@@ -10,7 +10,14 @@ class SearchInput {
     $searchInput.className = "SearchInput";
     $target.appendChild($header);
     $header.appendChild($searchInput);
+    this.$searchInput.focus();
 
+    $searchInput.addEventListener("click", (e) =>{
+      console.log('hhh');
+      if(e.target.value.length > 0){
+        e.target.value = "";
+      };
+    })
     $searchInput.addEventListener("keyup", e => {
       if (e.keyCode === 13) {
         onSearch(e.target.value);
