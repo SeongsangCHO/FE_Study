321238c567ff42adc6f8f482c52427ebf7ec03f9
Seongsangcho <secho@Seongsangchoui-MacBookAir.local>
2021-04-01 19:19:31 +0900

    api

--- 21-devmatch/src/api/Api.js
+++ 21-devmatch/src/api/Api.js
@@ -1,20 +1,33 @@
-import {END_POINT} from '../util/const.js';
+import { END_POINT, FILE_END_POINT } from "../util/const.js";
 
-const request = async(url) => {
-  try{
-    const response = await fetch(url);
-    return response.json();
-  }catch(error){
+//1. root 내용 가져오기
+
+//2. 특정디렉토리에 속하는 파일/ 디렉토리 불러오기
+//parent가 null이면 root디랙토라임.  1번 API랑 엔드포인트는 같고 마지막 id가 붙음
+//따로 나누어야하남
+
+const request = async (url) => {
+  try {
+    const result = await fetch(url);
+
+    if (!result.ok) {
+      throw new Error("ERROR");
+    } else {
+      //success
+    }
+  } catch (error) {
     console.error(error);
   }
-}
+};
 
-const api = {
-  getFileList :  () => {
-    return request(`${END_POINT}`);
+export const api = {
+  fetchContent: async () => {
+    return await request(`${END_POINT}`);
   },
-  getNestedList: (id) =>{
-    return request(`${END_POINT}/${id}`)
+  fetchContentById: async (id) => {
+    return await request(`${END_POINT}/${id}`);
+  },
+  fetchImage: async (filePath) => {
+    return await request(`${FILE_END_POINT}/${filePath}`);
   }
-}
-export default api;
+};
