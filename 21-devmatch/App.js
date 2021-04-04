dc0d0633251892a16b32d4398f650c4f0063e9c2
Seongsangcho <liasqui31@gmail.com>
2021-04-04 17:50:28 +0900

    prev feature done

--- 21-devmatch/App.js
+++ 21-devmatch/App.js
@@ -1,8 +1,7 @@
 import Breadcrumb from "./src/components/Breadcrumb.js";
 import ImageView from "./src/components/ImageView.js";
 import Nodes from "./src/components/Nodes.js";
-import {api} from './src/api/Api.js';
-
+import { api } from "./src/api/Api.js";
 
 //state 반영 -> render -> useEffect
 
@@ -22,9 +21,9 @@ export default class App {
         //   data: [{}, {}] // 1 id를 통해 얻어온 데이터들,
         // }
       },
-      pathIdList: null, // id를 통해 state.data객체의 name얻기 // 변경시 breadcrumb rerender
+      pathIdList: [], // id를 통해 state.data객체의 name얻기 // 변경시 breadcrumb rerender
       //뒤로가기시 path에서 뽑고 상위 path값(id)으로 currentPath 변경, => 랜더수행
-      currentPathId: "root", // 뒤로가기, dir클릭해서 이동할 때, id값 변경되면 node rerender
+      currentPathId: "root" // 뒤로가기, dir클릭해서 이동할 때, id값 변경되면 node rerender
     };
 
     this.render(); // App 랜더링시점. 하위 컴포넌트들 랜더링.
@@ -34,43 +33,58 @@ export default class App {
   render() {
     this.Breadcrumb = new Breadcrumb({
       $App: this.$App,
-      getState : () => this.state,
+      getState: () => this.state
     });
     this.Nodes = new Nodes({
       $App: this.$App,
-      getState : () => this.state,
-      getContentById : async (id) =>{
-        return await api.fetchContentById(id);
+      getState: () => this.state,
+      browsingSubDir: async (dirName, dirId) => {
+        const response = await api.fetchContentById(dirId);
+        this.setCurrentPathState(dirId);
+        this.setDataState({ [dirId]: { name: dirName, subDirData: response } });
+        this.setPathState([...this.state.pathIdList, dirId]);
       },
-      browsingSubDir : (name,id) =>{
-        const response = getContentById(id);
-        this.setDataState({[id]: { name: [name], subDirData: response}});
-      }
-
+      renderPrevDir: () => {
+        this.setPathState(this.popPathState()); //set, render까지,
+        this.setCurrentPathState(
+          this.state.pathIdList[this.state.pathIdList.length - 1]
+        ); //마지막 원소를 현재 path로 설정
+        this.Nodes.render();
+      },
     });
     this.ImageView = new ImageView({
       $App: this.$App
     });
   }
-  setDataState(newState){//{id : data( [{ }, { } ] )}
-    this.state.data = {...this.state.data, ...newState};
-    console.log('setDataState', newState.id, newState, this.state);
-    
+  setDataState(newState) {
+    //{id : data( [{ }, { } ] )}
+    this.state.data = { ...this.state.data, ...newState };
+    console.log("setDataState", newState.id, newState, this.state);
+
     this.Nodes.render();
   }
-  setPathState(newPathId){
+  setPathState(newPathId) {
     this.state.pathIdList = [...newPathId];
+    console.log(this.state.pathIdList);
+
     this.Breadcrumb.render();
   }
-  setCurrentPathState(currentPathId){
+  setCurrentPathState(currentPathId) {
     this.state.currentPathId = currentPathId;
-    this.Nodes.render();
+    // this.Nodes.render();
   }
 
-  initRootData(){
+  popPathState() {
+    if (this.state.currentPathId !== "root") {
+      return this.state.pathIdList.slice(0, this.state.pathIdList.length - 1);
+    }
+    return ;
+  }
+
+  initRootData() {
     api.fetchContent().then((res) => {
-      this.setDataState({root:{ name:"root", subDirData: res, }});
+      this.setDataState({ root: { name: "root", subDirData: res } });
       this.setPathState(["root"]);
-    })
+    });
   }
 }
--- 21-devmatch/src/components/Nodes.js
+++ 21-devmatch/src/components/Nodes.js
@@ -1,5 +1,7 @@
 export default class Nodes {
-  constructor({ $App, getState }) {
+  constructor({ $App, getState, browsingSubDir, renderPrevDir }) {
+    this.renderPrevDir = renderPrevDir;
+    this.browsingSubDir = browsingSubDir;
     this.getState = getState;
     this.$App = $App;
     this.init();
@@ -9,9 +11,13 @@ export default class Nodes {
 
   onClickNode(e) {
     const type = e.target.parentNode.dataset["type"];
+    const dirName = e.target.parentNode.dataset["name"];
+    const dirId = e.target.parentNode.dataset["id"];
+
     switch (type) {
       case "DIRECTORY": {
-        this.directoryHandler();
+        this.browsingSubDir(dirName, dirId);
+
         break;
       }
       case "FILE": {
@@ -19,21 +25,14 @@ export default class Nodes {
         break;
       }
       case "PREV": {
-        this.prevHandler();
+        this.renderPrevDir();
         break;
       }
       default:
-        return ;
+        return;
     }
-    this.directoryHandler();
-    this.fileHandler();
-    this.prevHandler();
   }
 
-  directoryHandler(){}
-  fileHandler(){}
-  prevHandler(){}
-  
   bindNodeClickEvent() {
     this.$nodesContainer.addEventListener("click", this.onClickNode.bind(this));
   }
@@ -44,19 +43,30 @@ export default class Nodes {
     this.$App.appendChild(this.$nodesContainer);
   }
 
+  renderPrevElement() {
+    return `
+    <div class="Node" data-type="PREV">
+      <img src="./assets/prev.png">
+    </div>
+    `;
+  }
   render() {
     const { data, currentPathId } = this.getState();
+    console.log(currentPathId);
+
     if (data[currentPathId] !== undefined) {
-      this.$nodesContainer.innerHTML = data[currentPathId].subDirData.map(
-        (data) => {
-          return `
-      <div class="Node" data-type="${data.type}">
+      this.$nodesContainer.innerHTML =
+        (currentPathId !== "root" ? this.renderPrevElement() : "") +
+        data[currentPathId].subDirData
+          .map((data) => {
+            return `
+      <div class="Node" data-id="${data.id}" data-name="${data.name}" data-type="${data.type}">
         <img src="./assets/${data.type}.png">
         <div>${data.name}</div>
       </div>
       `;
-        }
-      );
+          })
+          .join("");
     }
   }
 }
