cd6563e19915f5253fb0d881d813f519385d9663
Seongsangcho <liasqui31@gmail.com>
2021-06-19 21:21:57 +0900

    [Error] : API 겹치게 호출됨

--- infinite-scroll/infinite-scroll/src/App.js
+++ infinite-scroll/infinite-scroll/src/App.js
@@ -1,54 +1,89 @@
-import { useState, useEffect, useRef } from "react";
-import logo from "./logo.svg";
+import { useState, useEffect, useRef, useCallback } from "react";
 import axios from "axios";
+import "./App.css";
 const IMAGE_ENDPOINT = `https://image.tmdb.org/t/p/w500`;
 const API_ENDPOINT = `https://api.themoviedb.org/3/movie`;
 const API_KEY = `4c21dda5cc0e6817211c9d7d38ed5d1d`;
 
 function App() {
+  const interSectRef = useRef(null);
   const [id, setId] = useState(1);
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoding] = useState(false);
   useEffect(async () => {
-    const initData = await fetchData(1);
-    console.log(initData);
+    console.log("초기데이터 받아오기");
+
+    await getNextData(id);
   }, []);
-  let options = {
-    root: document.querySelector("#root"),
-    rootMargin: "0px",
-    threshold: 1.0,
+  const handleObserver = async (entries) => {
+    console.log("handle", "id", id);
+
+    const target = entries[0];
+    if (target.isIntersecting) {
+      console.log("intersect", id);
+      let res = await getNextData(id);
+
+      console.log(res);
+    }
   };
-  const getNextData = async () => {
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver, options);
+    if (interSectRef.current) observer.observe(interSectRef.current);
+    return () => observer.disconnect();
+  }, [data]);
+  let options = {
+    root: null,
+    // root: document.querySelector("#root"),
+    rootMargin: "20px",
+    threshold: 0.5,
+  };
+  const [getDataCnt, setGetDataCnt] = useState(0);
+  const getNextData = async (id) => {
+    console.log("call GND", data, id);
+
+    setIsLoding(true);
     const promiseContainer = [];
     try {
-      for (let i = id; i <= id + 5; i++) {
+      for (let i = id; i < id + 5; i++) {
         promiseContainer.push(fetchData(i));
       }
-      Promise.all(promiseContainer).then((value) => setData([...data, ...value]));
+      Promise.all(promiseContainer).then((value) => {
+        value = value.filter((v) => v !== undefined);
+        setTimeout(() => {
+          setIsLoding(false);
+          setData([...data, ...value]);
+          setGetDataCnt(value.length);
+        }, 1000);
+      });
     } catch {
       console.log("NOT FOUND");
     }
   };
-  const fetchData = async (page) => {
+  const fetchData = useCallback(async (page) => {
     try {
       const res = await axios.get(`${API_ENDPOINT}/${page}?api_key=${API_KEY}`);
-      // setData([...data, { uid: id, ...res.data }]);
-      setId((prev) => prev + 1);
+      setId(page + 1);
       return res.data;
     } catch {
       console.log("ERROR");
     }
-  };
-  const interSectRef = useRef(null);
+  }, []);
   return (
     <div className="App">
       infinite scroll
-      <button onClick={getNextData}>getData</button>
-      <div>
-        <h1>id</h1>
-        <h2>title</h2>
-        <h3>popularity</h3>
-        {/* <img>postPath</img> */}
-      </div>
+      <button onClick={() => getNextData(id)}>getData</button>
+      {data.map((obj) => {
+        return (
+          <div key={obj?.title}>
+            <h5>{obj?.id}</h5>
+            <h1>제목 : {obj?.title}</h1>
+            <img src={IMAGE_ENDPOINT + obj?.backdrop_path} />
+            <h3>평점 : {obj?.popularity}</h3>
+            <hr />
+          </div>
+        );
+      })}
+      {isLoading ? <div className="box">box</div> : ""}
       <div ref={interSectRef}>InterSection Point</div>
     </div>
   );
