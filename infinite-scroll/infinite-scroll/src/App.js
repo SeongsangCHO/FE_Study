61ecfcd6114a8a679c5d9e93df513dd4775c76de
Seongsangcho <liasqui31@gmail.com>
2021-06-19 22:28:40 +0900

    done

--- infinite-scroll/infinite-scroll/src/App.js
+++ infinite-scroll/infinite-scroll/src/App.js
@@ -1,90 +1,81 @@
 import { useState, useEffect, useRef, useCallback } from "react";
 import axios from "axios";
 import "./App.css";
+import errorImg from "./42.png";
 const IMAGE_ENDPOINT = `https://image.tmdb.org/t/p/w500`;
 const API_ENDPOINT = `https://api.themoviedb.org/3/movie`;
 const API_KEY = `4c21dda5cc0e6817211c9d7d38ed5d1d`;
 
 function App() {
   const interSectRef = useRef(null);
-  const [id, setId] = useState(1);
+  const [id, setId] = useState(2);
   const [data, setData] = useState([]);
   const [isLoading, setIsLoding] = useState(false);
   useEffect(async () => {
-    console.log("초기데이터 받아오기");
-
-    await getNextData(id);
-  }, []);
-  const handleObserver = async (entries) => {
-    console.log("handle", "id", id);
-
+    await fetchData(id);
+  }, [id]);
+  const handleObserver = useCallback(async (entries) => {
     const target = entries[0];
     if (target.isIntersecting) {
-      console.log("intersect", id);
-      let res = await getNextData(id);
-
-      console.log(res);
+      console.log("is InterSecting");
+      setId((prev) => prev + 1);
     }
-  };
+  }, []);
   useEffect(() => {
     const observer = new IntersectionObserver(handleObserver, options);
     if (interSectRef.current) observer.observe(interSectRef.current);
     return () => observer.disconnect();
-  }, [data]);
+  }, [handleObserver]);
   let options = {
     root: null,
-    // root: document.querySelector("#root"),
     rootMargin: "20px",
-    threshold: 0.5,
+    threshold: 1.0,
   };
-  const [getDataCnt, setGetDataCnt] = useState(0);
-  const getNextData = async (id) => {
-    console.log("call GND", data, id);
-
-    setIsLoding(true);
-    const promiseContainer = [];
-    try {
-      for (let i = id; i < id + 5; i++) {
-        promiseContainer.push(fetchData(i));
+  const fetchData = useCallback(
+    async (page) => {
+      setIsLoding(true);
+      try {
+        const res = await axios.get(
+          `${API_ENDPOINT}/${page}?api_key=${API_KEY}`
+        );
+        setData([...data].concat({ ...res.data }));
+        setIsLoding(false);
+        return res.data;
+      } catch {
+        console.log("ERROR");
+        setId((prev) => prev + 1);
       }
-      Promise.all(promiseContainer).then((value) => {
-        value = value.filter((v) => v !== undefined);
-        setTimeout(() => {
-          setIsLoding(false);
-          setData([...data, ...value]);
-          setGetDataCnt(value.length);
-        }, 1000);
-      });
-    } catch {
-      console.log("NOT FOUND");
-    }
-  };
-  const fetchData = useCallback(async (page) => {
-    try {
-      const res = await axios.get(`${API_ENDPOINT}/${page}?api_key=${API_KEY}`);
-      setId(page + 1);
-      return res.data;
-    } catch {
-      console.log("ERROR");
-    }
-  }, []);
+    },
+    [data]
+  );
   return (
     <div className="App">
-      infinite scroll
-      <button onClick={() => getNextData(id)}>getData</button>
-      {data.map((obj) => {
-        return (
-          <div key={obj?.title}>
-            <h5>{obj?.id}</h5>
-            <h1>제목 : {obj?.title}</h1>
-            <img src={IMAGE_ENDPOINT + obj?.backdrop_path} />
-            <h3>평점 : {obj?.popularity}</h3>
-            <hr />
-          </div>
-        );
-      })}
-      {isLoading ? <div className="box">box</div> : ""}
-      <div ref={interSectRef}>InterSection Point</div>
+      <header>
+        <h1>Movie Infinite Scroll</h1>
+      </header>
+      <div className="Wrapper">
+        {data.map((obj) => {
+          let imgPath = IMAGE_ENDPOINT + (obj.poster_path ?? obj.backdrop_path);
+
+          if (imgPath.substr(-4) === "null") {
+            imgPath = errorImg;
+          }
+
+          return (
+            <div className="box" key={obj?.title}>
+              <div>
+                <h1>{obj?.title}</h1>
+                <img src={imgPath} />
+                <span>평점 : {obj?.popularity}</span>
+              </div>
+            </div>
+          );
+        })}
+        {isLoading ? <div className="box">box</div> : ""}
+        <div className="inter" ref={interSectRef}>
+          <h5>더보기</h5>
+        </div>
+      </div>
     </div>
   );
 }
